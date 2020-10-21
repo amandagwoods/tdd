5f98f984185b69afcb0a7864f026f6b054cfe30a
Amanda Woods <amanda.garrison.woods@gmail.com>
2020-10-21 09:40:05 -0500

    feat: first push with warmup

---
+++ src/main/java/Device.java
@@ -0,0 +1,17 @@
+public class Device {
+
+    private String name;
+
+    public Device() {
+
+    }
+
+    public Device(String iPhone) {
+        this.name = iPhone;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+}
---
+++ src/test/java/MyFirstTest.java
@@ -0,0 +1,130 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class MyFirstTest {
+
+    private List<String> names;
+
+
+    @Before
+    public void setUp(){
+        this.names = new ArrayList<>();
+        this.names.add("Fer");
+    }
+
+    @Test
+    public void testIfNamesIsInitialized(){
+        assertNotNull(names);
+    }
+
+    @Test
+    public void testIfANameCanBeAdded(){
+        assertEquals(1, this.names.size());
+        this.names.add("Zach");
+        assertEquals(2, this.names.size());
+        assertSame("fer", this.names.get(0));
+        assertSame("Zach", this.names.get(1));
+    }
+
+
+    @Test
+    public void testIfCompanyNameIsEquals() {
+        String expected = "Codeup";
+        String actual = "Codeup";
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testIfChangeIsCorrect() {
+        Double price = 10.0;
+        Double discount = 4.5;
+
+        assertEquals(5.5, price - discount, 0);
+        assertEquals(5.1, price - discount, 0.5);
+        assertNotEquals(4.9, price - discount, 0.5);
+    }
+
+    @Test
+    public void testIfObjectsAreDifferent() {
+        Object dog = new Object();
+        Object sheep = new Object();
+        Object clonnedSheep = sheep;
+
+        assertNotSame(sheep, dog);
+        assertSame(sheep, clonnedSheep);
+    }
+
+    @Test
+    public void testIfArrayEquals() {
+        char[] expected = {'J','u','n','i','t'};
+        char[] actual = "Junit".toCharArray();
+
+        assertArrayEquals(expected, actual);
+    }
+
+
+
+
+    @Test
+    public void testIfGreaterOrLesserThanWorks() {
+
+        boolean learningTDD = true;
+
+        assertTrue(learningTDD);
+        assertTrue("5 is greater than 4", 5 > 4);
+        assertFalse("5 is not greater than 6", 5 > 6);
+    }
+
+    @Test
+    public void testIfInstanceIsNull() {
+        Device phone = new Device();
+        Device laptop = null;
+        assertNull(null);
+        assertNotNull(phone);
+    }
+
+    @Test
+    public void testIfDeviceNamePossible(){
+        Device phone = new Device("iPhone");
+        assertEquals("iPhone", phone.getName());
+
+    }
+
+    @Test
+    public void testIfNumArrayEqual() {
+        int[] numbers = {1, 2, 3};
+        int[] otherNumbers = new int[3];
+        otherNumbers[0] = 1;
+        otherNumbers[1] = 2;
+        otherNumbers[2] = 3;
+
+        assertArrayEquals(numbers,otherNumbers);
+    }
+
+    @Test
+    public void testArraysNotSame(){
+        List<String> languages = new ArrayList<>();
+        List<String> moreLanguages = new ArrayList<>();
+
+        assertNotSame(languages, moreLanguages);
+    }
+
+    @Test
+    public void testStringContains(){
+        String language = "PHP";
+        language.contains("H"); // use assertTrue for this statement
+        language.contains("J"); // use assertFalse for this statement
+
+        assertTrue(language.contains("H"));
+        assertFalse(language.contains("J"));
+    }
+
+
+    }
+
